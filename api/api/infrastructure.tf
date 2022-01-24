c1a0b7f176767ef497f53079c14f996cbba83ead
Tashik <k180142@nu.edu.pk>
2022-01-24 06:55:21 +0500

    updated

---
+++ api/api/infrastructure.tf
@@ -0,0 +1,58 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+}
+
+resource "azurerm_resource_group" "local-terraform-rg" {
+  name     = "terraform-rg"
+  location = "uaenorth"
+}
+
+resource "azurerm_container_group" "local-container-group" {
+  name                = "api-container"
+  location            = azurerm_resource_group.local-terraform-rg.location
+  resource_group_name = azurerm_resource_group.local-terraform-rg.name
+  ip_address_type     = "public"
+  dns_name_label      = "iac-api"
+  os_type             = "Linux"
+
+  container {
+    name   = "api-container"
+    image  = "tashikmoin/iac"
+    cpu    = 1
+    memory = 1
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
+
+variable "ARM_CLIENT_ID" {
+
+}
+
+variable "ARM_CLIENT_SECRET" {
+
+}
+
+variable "ARM_TENANT_ID" {
+
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+
+}
