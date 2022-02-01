38d23111da7f6ba0bea081a90b1424abc19761d3
Tashik <k180142@nu.edu.pk>
2022-02-01 23:19:28 +0500

    updated

--- api/api/Controllers/WeatherForecastController.cs
+++ api/api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "TASHIKKKKKKKKKKKKKKKKKKKKK", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
