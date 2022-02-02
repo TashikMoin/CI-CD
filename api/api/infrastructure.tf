e4b8c4414feb87bf7701bea7279b7e57c739b604
Tashik <k180142@nu.edu.pk>
2022-02-03 02:25:56 +0500

    updated

--- api/api/infrastructure.tf
+++ api/api/infrastructure.tf
@@ -1,51 +1,51 @@
-# uncommit the file to provision the infrastructure
+uncommit the file to provision the infrastructure
 
 
-# variable "ARM_CLIENT_ID" {
+variable "ARM_CLIENT_ID" {
 
-# }
+}
 
-# variable "ARM_CLIENT_SECRET" {
+variable "ARM_CLIENT_SECRET" {
 
-# }
+}
 
-# variable "ARM_TENANT_ID" {
+variable "ARM_TENANT_ID" {
 
-# }
+}
 
-# variable "ARM_SUBSCRIPTION_ID" {
+variable "ARM_SUBSCRIPTION_ID" {
 
-# }
+}
 
-# variable "imagetag" {
-#   type        = string
-#   description = "Latest build using the latest image tag."
-# }
-
-
-# terraform {
-
-#   backend "azurerm" {}
-
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=2.91.0"
-#     }
-#   }
-
-# }
+variable "imagetag" {
+  type        = string
+  description = "Latest build using the latest image tag."
+}
 
 
+terraform {
+
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+
+}
 
 
-# provider "azurerm" {
-#   features {}
-#   subscription_id = var.ARM_SUBSCRIPTION_ID
-#   client_id       = var.ARM_CLIENT_ID
-#   client_secret   = var.ARM_CLIENT_SECRET
-#   tenant_id       = var.ARM_TENANT_ID
-# }
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+}
 
 # resource "azurerm_resource_group" "local-terraform-rg" {
 #   name     = "terraform-rg"
